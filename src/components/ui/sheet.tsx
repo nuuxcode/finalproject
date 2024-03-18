d66019af7f741ec55c808df3e16f903c43e0b8e3
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-03-18 09:57:39 +0000

    fix: module dependencies

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
 import { cva, type VariantProps } from "class-variance-authority"
-import { X } from "lucide-react"
+import { FaTimes as X } from "react-icons/fa";
 
 import { cn } from "~/lib/utils"
 
