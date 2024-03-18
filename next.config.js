dac1fa6f8a4980f0ba2fad3e11e7e0eb11538751
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-03-18 23:12:27 +0000

    feat: added image cdn pattern

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ui-avatars.com',
+            }
+        ]
+    }
+};
 
 export default config;
