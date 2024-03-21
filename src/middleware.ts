90379b2d76818d283400f147926c548452842801
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-03-21 03:32:18 +0000

    clean branch

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({
-  publicRoutes: ["/api/trpc(.*)", "/", "/u/(.*)"],
-});
-
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
