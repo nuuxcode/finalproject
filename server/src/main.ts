d673491fea6f7d56064397a7ddfbb568b37fb3ff
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-03-21 04:40:50 +0000

    Initial repo Next, Nest, Prisma, Swagger

---
+++ server/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('My API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(4000);
+}
+bootstrap();
