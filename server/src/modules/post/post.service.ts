d8082078525bf7a915a672a9106b71f1d7a8d632
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-03-27 14:30:49 +0000

    feat(post.service): add full-text search and include user and attachment info in findOne and findAll

--- server/src/modules/post/post.service.ts
+++ server/src/modules/post/post.service.ts
@@ -2,16 +2,58 @@ import { Injectable } from '@nestjs/common';
 import { Post, Prisma } from '@prisma/client';
 
 import { PrismaService } from '../prisma/prisma.service';
+import { Post as PostModel } from '@prisma/client';
 
 @Injectable()
 export class PostService {
   constructor(private prisma: PrismaService) {}
+
+  async searchPosts(searchTerm: string): Promise<PostModel[]> {
+    const posts = await this.prisma.$queryRaw`
+      SELECT *
+      FROM "Post"
+      WHERE to_tsvector('english', title) @@ plainto_tsquery('english', ${searchTerm})
+      OR to_tsvector('english', content) @@ plainto_tsquery('english', ${searchTerm})
+    `;
+
+    return (posts as PostModel[]).map((post) => {
+      return {
+        id: post.id,
+        title: post.title,
+        content: post.content,
+        userId: post.userId,
+        forumId: post.forumId,
+        isPinned: post.isPinned,
+        isVisible: post.isVisible,
+        slug: post.slug,
+        createdAt: post.createdAt,
+        updatedAt: post.updatedAt,
+        commentsCount: post.commentsCount,
+        viewsCount: post.viewsCount,
+        votesCount: post.votesCount,
+      } as PostModel;
+    });
+  }
 
   async findOne(
     postWhereUniqueInput: Prisma.PostWhereUniqueInput,
   ): Promise<Post | null> {
     return this.prisma.post.findUnique({
       where: postWhereUniqueInput,
+      include: {
+        user: {
+          select: {
+            username: true,
+            avatarUrl: true,
+            reputation: true,
+          },
+        },
+        attachments: {
+          include: {
+            attachment: true,
+          },
+        },
+      },
     });
   }
 
@@ -29,6 +71,20 @@ export class PostService {
       cursor,
       where,
       orderBy,
+      include: {
+        user: {
+          select: {
+            username: true,
+            avatarUrl: true,
+            reputation: true,
+          },
+        },
+        attachments: {
+          include: {
+            attachment: true,
+          },
+        },
+      },
     });
   }
 
