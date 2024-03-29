07653ea6b63e2cb3f9200dd3e21131fce41cce9e
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-03-29 19:38:15 +0000

    feat: disabled image optimization and added some deps

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'ui-avatars.com',
-            }
-        ]
+        unoptimized: true,
     }
 };
 
