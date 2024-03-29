845a7adf91dce1c4f661dbe5a536ae49f2bb7bb0
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-03-29 19:40:39 +0000

    feat: refactor page and add new component for posts

--- client/src/components/posts/posts.tsx
+++ client/src/components/posts/posts.tsx
@@ -1,5 +1,27 @@
+'use client'
+
+import { useFetcher } from "~/hooks/fetcher";
+import useSWR from "swr";
+import { useUser } from "@clerk/clerk-react";
+import Post from "./post";
+import { Separator } from "../ui/separator";
+
+
+
 export default function Posts() {
-    return (<div>
-    {/*  TODO Create a filter tab   */}
-    </div>)
+  const { allPosts } = useFetcher();
+  const { isSignedIn } = useUser();
+
+  const { data, error } = useSWR("/posts", allPosts);
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between px-4 md:px-8 py-10 gap-8">
+            {data && data.map((post) => (
+                <>
+                <Post key={post.id} post={post} />
+                <Separator className="bg-[#64748B] bg-opacity-30 h-[2px]" />
+                </>
+            ))}
+        </main>
+    )
 }
