46990b78b1962563b032619af9c79341bba33216
victor langat <provicml@gmail.com>
2024-03-30 16:02:18 +0300

    feat: add CreateNewCommentDto to handle creating new comments

--- server/src/modules/comment/dto/create-comment.dto.ts
+++ server/src/modules/comment/dto/create-comment.dto.ts
@@ -1,11 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
 
+export class CreateNewCommentDto {
+  @ApiProperty()
+  content: string;
+
+  @ApiProperty()
+  authorEmail: string;
+}
+
 export class CreateCommentDto {
   @ApiProperty()
   content: string;
 
   @ApiProperty()
   postId: string;
+
+  @ApiProperty()
+  authorEmail: string;
 
   @ApiProperty()
   parentId?: string;
