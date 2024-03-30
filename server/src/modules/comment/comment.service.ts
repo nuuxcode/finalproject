8b7fec6c6461e15e9935c040dd5fe4c11917bfc1
victor langat <provicml@gmail.com>
2024-03-30 16:05:18 +0300

    fix:  bug in login functionality

--- server/src/modules/comment/comment.service.ts
+++ server/src/modules/comment/comment.service.ts
@@ -2,7 +2,9 @@ import {
   Injectable,
   BadRequestException,
   UnauthorizedException,
+  NotFoundException,
 } from '@nestjs/common';
+import { Comment as CommentModel } from '@prisma/client';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 import { PrismaService } from '../prisma/prisma.service';
@@ -10,10 +12,248 @@ import { Comment } from './comment.entity';
 // import { AttachmentMetadataDto } from '../attachment/attachment.dto';
 // import { AttachmentMetadataDto } from './dto/add-attachment.dto';
 import { commentAttachDto } from './dto/comment-attach.dto';
+// import { emailAddresses } from '@clerk/clerk-sdk-node';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class CommentService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async getCommentsForPost(postId: string): Promise<Comment[]> {
+    try {
+      const comments = await this.prisma.comment.findMany({
+        where: {
+          postId,
+          parentId: null,
+        },
+        include: {
+          user: {
+            select: {
+              username: true,
+              avatarUrl: true,
+              reputation: true,
+            },
+          },
+        },
+      });
+      return comments;
+    } catch (error) {
+      console.error(error);
+      throw new BadRequestException('Failed to fetch comments');
+    }
+  }
+
+  async getCommentsReplies(
+    commentId: string,
+    page: number = 1,
+    limit: number = 10,
+  ): Promise<Comment[]> {
+    try {
+      const childComments = await this.prisma.comment.findMany({
+        where: {
+          parentId: commentId,
+        },
+        include: {
+          user: {
+            select: {
+              username: true,
+              avatarUrl: true,
+              reputation: true,
+            },
+          },
+          attachments: true,
+        },
+        skip: (page - 1) * limit,
+        take: limit,
+      });
+      return childComments;
+    } catch (error) {
+      console.error(error);
+      throw new BadRequestException('Failed to fetch child comments');
+    }
+  }
+
+  async getCommentById(commentId: string): Promise<Comment> {
+    try {
+      const comment = await this.prisma.comment.findUnique({
+        where: {
+          id: commentId,
+        },
+        include: {
+          user: {
+            select: {
+              username: true,
+              avatarUrl: true,
+              reputation: true,
+            },
+          },
+          attachments: true,
+        },
+      });
+      if (!comment) {
+        throw new BadRequestException('Comment not found');
+      }
+      return comment;
+    } catch (error) {
+      console.error(error);
+      throw new BadRequestException('Failed to fetch comment');
+    }
+  }
+
+  async getAllComments(): Promise<Comment[]> {
+    try {
+      const comments = await this.prisma.comment.findMany({
+        where: {
+          isVisible: true,
+        },
+        include: {
+          user: {
+            select: {
+              username: true,
+              avatarUrl: true,
+              reputation: true,
+            },
+          },
+        },
+      });
+      return comments;
+    } catch (error) {
+      console.error(error);
+      throw new BadRequestException('Failed to fetch comments');
+    }
+  }
+
+  async searchComments(searchTerm: string): Promise<CommentModel[]> {
+    const comments = await this.prisma.$queryRaw`
+      SELECT *
+      FROM "Comment"
+      WHERE to_tsvector('english', content) @@ plainto_tsquery('english', ${searchTerm})
+    `;
+
+    return (comments as CommentModel[]).map((comment) => {
+      return {
+        id: comment.id,
+        content: comment.content,
+        userId: comment.userId,
+        postId: comment.postId,
+        isVisible: comment.isVisible,
+        parentId: comment.parentId,
+        createdAt: comment.createdAt,
+        updatedAt: comment.updatedAt,
+        VotesCount: comment.VotesCount,
+        status: comment.status,
+      } as CommentModel;
+    });
+  }
+
+  async createNewComment(
+    data: Prisma.CommentCreateInput,
+  ): Promise<CommentModel> {
+    try {
+      const comment = await this.prisma.comment.create({
+        data,
+      });
+      return comment;
+    } catch (error) {
+      console.error(error);
+      throw new BadRequestException('Failed to create comment');
+    }
+  }
+
+  async updateCommentById(
+    params: {
+      where: Prisma.CommentWhereUniqueInput;
+      data: Prisma.CommentUpdateInput;
+    },
+    // id: string, updateCommentDto: Prisma.CommentUpdateInput
+  ): Promise<Comment> {
+    try {
+      const { data, where } = params;
+      const updatedComment = await this.prisma.comment.update({
+        where,
+        data,
+      });
+      return updatedComment;
+    } catch (error) {
+      console.error(error);
+      throw new BadRequestException('Failed to update comment');
+    }
+  }
+
+  async updateCommentVotes(
+    commentId: string,
+    voteStatus: number,
+  ): Promise<Comment> {
+    try {
+      const updatedComment = await this.prisma.comment.update({
+        where: {
+          id: commentId,
+        },
+        data: {
+          VotesCount: {
+            increment: voteStatus,
+          },
+        },
+      });
+      return updatedComment;
+    } catch (error) {
+      console.error(error);
+      throw new BadRequestException('Failed to update comment votes');
+    }
+  }
+
+  async updateCommentIsPublic(
+    commentId: string,
+    publish: boolean,
+  ): Promise<Comment> {
+    try {
+      const comment = await this.prisma.comment.findUnique({
+        where: {
+          id: commentId,
+        },
+      });
+      if (!comment) {
+        throw new NotFoundException('Comment not found');
+      }
+      if (comment.isVisible === publish) {
+        return comment;
+      }
+      const updatedComment = await this.prisma.comment.update({
+        where: {
+          id: commentId,
+        },
+        data: {
+          isVisible: publish,
+        },
+      });
+      return updatedComment;
+    } catch (error) {
+      console.error(error);
+      throw new BadRequestException('Failed to update comment status');
+    }
+  }
+  async deleteCommentById(commentId: string): Promise<Comment> {
+    try {
+      const comment = await this.prisma.comment.findUnique({
+        where: {
+          id: commentId,
+        },
+      });
+      if (!comment) {
+        throw new NotFoundException('Comment not found');
+      }
+      const deleted = await this.prisma.comment.delete({
+        where: {
+          id: comment.id,
+        },
+      });
+      return deleted;
+    } catch (error) {
+      console.error(error);
+      throw new BadRequestException('Failed to delete comment');
+    }
+  }
+
   async create(addCommentDto: CreateCommentDto, user: any): Promise<Comment> {
     const { content, postId, parentId } = addCommentDto;
     // const userId = user.id;
@@ -35,7 +275,7 @@ export class CommentService {
           },
           user: {
             connect: {
-              id: userId, // update for clerkauth 💡
+              id: userId,
             },
           },
           ...(parentId && {
@@ -105,37 +345,35 @@ export class CommentService {
     }
   }
 
-  async findAll(postId: string): Promise<Comment[]> {
-    try {
-      // Retrieve comments at the top level
-      const topLevelComments = await this.prisma.comment.findMany({
-        where: {
-          postId,
-          parentId: null, // top level comments wont have parenrID
-        },
-        include: {
-          user: true,
-          post: {
-            select: {
-              userId: true,
-              title: true,
-            },
+  async findAll(params: {
+    page?: number;
+    take?: number;
+    cursor?: Prisma.CommentWhereUniqueInput;
+    where?: Prisma.CommentWhereInput;
+    orderBy?: Prisma.CommentOrderByWithAggregationInput;
+  }): Promise<CommentModel[]> {
+    const { page = 0, take = 10, cursor, where, orderBy } = params;
+    return this.prisma.comment.findMany({
+      skip: page * take,
+      take,
+      cursor,
+      where,
+      orderBy,
+      include: {
+        user: {
+          select: {
+            username: true,
+            avatarUrl: true,
+            reputation: true,
           },
-          replies: {
-            include: {
-              user: true,
-            },
-            orderBy: { createdAt: 'desc' },
+        },
+        attachments: {
+          include: {
+            attachment: true,
           },
         },
-        orderBy: { createdAt: 'desc' },
-      });
-
-      return topLevelComments;
-    } catch (error) {
-      console.error(error);
-      throw new BadRequestException('Failed to fetch comments');
-    }
+      },
+    });
   }
 
   async update(id: string, updateCommentDto: UpdateCommentDto, user: any) {
@@ -180,47 +418,6 @@ export class CommentService {
       });
 
       return newComment;
-    }
-  }
-
-  async remove(id: string, user: any): Promise<boolean> {
-    try {
-      const userId = user.id;
-      const isAdmin = user.isAdmin;
-
-      const comment = await this.prisma.comment.findUnique({
-        where: {
-          id,
-        },
-        // to also delete nested comments if needed
-        // include: {
-        //   replies: true,
-        // },
-      });
-
-      if (!comment) {
-        throw new Error('Comment not found');
-      }
-
-      if (comment.userId !== userId && !isAdmin) {
-        throw new Error("Unauthorized: Cannot delete another user's comment");
-      }
-
-      // Soft deleting the comment by setting its status to 'deleted': comment and children kept but not displayed
-      // deletedAt field in database can be added to alsmo mark deleted comments
-      await this.prisma.comment.update({
-        where: {
-          id,
-        },
-        data: {
-          status: 'deleted',
-        },
-      });
-
-      return true;
-    } catch (error) {
-      console.error(error);
-      throw new BadRequestException('Failed to delete comment');
     }
   }
 }
