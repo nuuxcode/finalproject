63ffba142f79b19fb14b0a349619eb5a98b733aa
victor langat <provicml@gmail.com>
2024-03-30 16:03:57 +0300

    feat: update comment dto for updates on child comments

--- server/src/modules/comment/dto/update-comment.dto.ts
+++ server/src/modules/comment/dto/update-comment.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateCommentDto } from './create-comment.dto';
+import { CreateCommentDto, CreateNewCommentDto } from './create-comment.dto';
 
 export class UpdateCommentDto extends PartialType(CreateCommentDto) {}
+export class UpdateNewCommentDto extends PartialType(CreateNewCommentDto) {}
