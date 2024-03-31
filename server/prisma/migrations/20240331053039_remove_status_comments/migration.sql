d89de28e998bd80fe9e5f823dc24ca0833caea16
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-03-31 09:10:00 +0000

    feat(server): add migration files to remove status from comments

---
+++ server/prisma/migrations/20240331053039_remove_status_comments/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `status` on the `Comment` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Comment" DROP COLUMN "status";
