bf2c6062725bdcde80c28d7da48844041930c487
STAGIAIRE2 <STAGIAIRE2@nutritechagro.com>
2024-04-02 16:41:26 +0000

    installing the CSS tailwind

--- Admin/src/App.tsx
+++ Admin/src/App.tsx
@@ -6,7 +6,7 @@ import {
   useNotificationProvider,
   RefineSnackbarProvider,
   ThemedLayoutV2,
-  RefineThemes
+  RefineThemes,
 } from "@refinedev/mui";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -18,35 +18,30 @@ import routerBindings, {
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
-// import { DashboardPage } from "./pages/dashboard";
 import Dashboard from "@mui/icons-material/Dashboard";
 import { useTranslation } from "react-i18next";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { authProvider } from "./authProvider";
 import { Header } from "./components/header";
 import { ColorModeContextProvider } from "./contexts/color-mode";
-import {
-  PostCreate,
-  PostEdit,
-  PostList,
-  PostShow,
-} from "./pages/posts";
-import {
- UsersCreate,
- UsersEdit,
-  UsersShow,
-  UsersList,
-} from "./pages/users";
+import { PostCreate, PostEdit, PostList, PostShow } from "./pages/posts";
+import { UsersCreate, UsersEdit, UsersShow, UsersList } from "./pages/users";
 
 import { ForgotPassword } from "./pages/forgotPassword";
 import { Login } from "./pages/login";
 import { Register } from "./pages/register";
-import { CommentList, CommentCreate, CommentEdit, CommentShow } from "./pages/comments";
+import {
+  CommentList,
+  CommentCreate,
+  CommentEdit,
+  CommentShow,
+} from "./pages/comments";
 import { ForumList, ForumCreate, ForumEdit, ForumShow } from "./pages/forums";
 import { DashboardOutlined } from "@mui/icons-material";
 // import { Dashboard } from "./pages/dashboard";
 // import { ReportList, ReportCreate, ReportEdit, ReportShow } from "./pages/reports";
 import jsonServerDataProvider from "@refinedev/simple-rest";
+import DashboardPage from "./pages/dashboard";
 function App() {
   // const API_URL = "https://api.finefoods.refine.dev";
   const API_URL = "http://104.248.142.226:4000/api/v1";
@@ -58,7 +53,7 @@ function App() {
     changeLocale: (lang: string) => i18n.changeLanguage(lang),
     getLocale: () => i18n.language,
   };
-  
+
   return (
     <BrowserRouter>
       <GitHubBanner />
@@ -67,161 +62,161 @@ function App() {
           <CssBaseline />
           <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
           <RefineSnackbarProvider>
-          <ThemeProvider theme={RefineThemes.YellowDark}>
-            <Refine
-              // dataProvider={dataProvider("http://104.248.142.226:4000/api/v1")}
-              dataProvider={dataProvider}
-              // dataProvider={customDataProvider}
-              notificationProvider={useNotificationProvider}
-              routerProvider={routerBindings}
-              authProvider={authProvider}
-              i18nProvider={i18nProvider}
-              resources={[
-                {
-                  name: "dashboard",
-                  list: "/",
-                  meta: {
-                    label: "Dashboard",
-                    icon: <Dashboard />,
+            <ThemeProvider theme={RefineThemes.YellowDark}>
+              <Refine
+                // dataProvider={dataProvider("http://104.248.142.226:4000/api/v1")}
+                dataProvider={dataProvider}
+                // dataProvider={customDataProvider}
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerBindings}
+                authProvider={authProvider}
+                i18nProvider={i18nProvider}
+                resources={[
+                  {
+                    name: "dashboard",
+                    list: "/",
+                    meta: {
+                      label: "Dashboard",
+                      icon: <Dashboard />,
+                    },
                   },
-                },
-                {
-                  name: "posts",
-                  list: "/posts",
-                  create: "/posts/create",
-                  edit: "/posts/edit/:id",
-                  show: PostShow,
-                  meta: {
-                    canDelete: true,
+                  {
+                    name: "posts",
+                    list: "/posts",
+                    create: "/posts/create",
+                    edit: "/posts/edit/:id",
+                    show: PostShow,
+                    meta: {
+                      canDelete: true,
+                    },
+                    // showPath: (id: string) => `/posts/post/${id}`,
                   },
-                  // showPath: (id: string) => `/posts/post/${id}`,
-                },
-                {
-                  name: "users",
-                  list: "/users",
-                  create: "/users/create",
-                  edit: "/users/edit/:id",
-                  show: "/users/show/:id",
-                  meta: {
-                    canDelete: true,
+                  {
+                    name: "users",
+                    list: "/users",
+                    create: "/users/create",
+                    edit: "/users/edit/:id",
+                    show: "/users/show/:id",
+                    meta: {
+                      canDelete: true,
+                    },
                   },
-                },
-                {
-                  name: "comments",
-                  list: "/comments",
-                  create: "/comments/create",
-                  edit: "/comments/edit/:id",
-                  show: "/comments/show/:id",
-                  meta: {
-                    canDelete: true,
+                  {
+                    name: "comments",
+                    list: "/comments",
+                    create: "/comments/create",
+                    edit: "/comments/edit/:id",
+                    show: "/comments/show/:id",
+                    meta: {
+                      canDelete: true,
+                    },
                   },
-                },
-                {
-                  name: "forums",
-                  list: "/forums",
-                  create: "/forums/create",
-                  edit: "/forums/edit/:id",
-                  show: "/forums/show/:id",
-                  meta: {
-                    canDelete: true,
+                  {
+                    name: "forums",
+                    list: "/forums",
+                    create: "/forums/create",
+                    edit: "/forums/edit/:id",
+                    show: "/forums/show/:id",
+                    meta: {
+                      canDelete: true,
+                    },
                   },
-                },
-                // {
-                //   name: "reports",
-                //   list: "/reports",
-                //   create: "/reports/create",
-                //   edit: "/reports/edit/:id",
-                //   show: "/reports/show/:id",
-                //   meta: {
-                //     canDelete: true,
-                //   },
-                // },
-                {
-                  name: "tags",
-                  list: "/tags",
-                  create: "/tags/create",
-                  edit: "/tags/edit/:id",
-                  show: "/tags/show/:id",
-                  meta: {
-                    canDelete: true,
+                  // {
+                  //   name: "reports",
+                  //   list: "/reports",
+                  //   create: "/reports/create",
+                  //   edit: "/reports/edit/:id",
+                  //   show: "/reports/show/:id",
+                  //   meta: {
+                  //     canDelete: true,
+                  //   },
+                  // },
+                  {
+                    name: "tags",
+                    list: "/tags",
+                    create: "/tags/create",
+                    edit: "/tags/edit/:id",
+                    show: "/tags/show/:id",
+                    meta: {
+                      canDelete: true,
+                    },
                   },
-                },
-              ]}
-              options={{
-                syncWithLocation: true,
-                warnWhenUnsavedChanges: true,
-              }}
-            >
-              <Routes>
-                <Route
-                  element={
-                    <Authenticated
-                      key="authenticated-routes"
-                      fallback={<CatchAllNavigate to="/login" />}
-                    >
-                      <ThemedLayoutV2 Header={() => <Header sticky />}>
-                        <Outlet />
-                      </ThemedLayoutV2>
-                    </Authenticated>
-                  }
-                >
+                ]}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                }}
+              >
+                <Routes>
                   <Route
-                    index
-                    element={<NavigateToResource resource="dashboard" />}
-                  />
-                  <Route path="/">
-                      <Route index element={<Dashboard />} />
-                  </Route>
-                  <Route path="/posts">
-                    <Route index element={<PostList />} />
-                    <Route path="create" element={<PostCreate />} />
-                    <Route path="edit/:id" element={<PostEdit />} />
-                    <Route path="show/:id" element={<PostShow />} />
-                  </Route>
-                  <Route path="/users">
-                    <Route index element={< UsersList />} />
-                    <Route path="create" element={< UsersCreate />} />
-                    <Route path="edit/:id" element={< UsersEdit />} />
-                    <Route path="show/:id" element={< UsersShow />} />
-                  </Route>
-                  {/* <Route path="/reports">
+                    element={
+                      <Authenticated
+                        key="authenticated-routes"
+                        fallback={<CatchAllNavigate to="/login" />}
+                      >
+                        <ThemedLayoutV2 Header={() => <Header sticky />}>
+                          <Outlet />
+                        </ThemedLayoutV2>
+                      </Authenticated>
+                    }
+                  >
+                    <Route path="/">
+                      <Route index element={<DashboardPage />} />
+                    </Route>
+
+                    <Route path="/posts">
+                      <Route index element={<PostList />} />
+                      <Route path="create" element={<PostCreate />} />
+                      <Route path="edit/:id" element={<PostEdit />} />
+                      <Route path="show/:id" element={<PostShow />} />
+                    </Route>
+                    <Route path="/users">
+                      <Route index element={<UsersList />} />
+                      <Route path="create" element={<UsersCreate />} />
+                      <Route path="edit/:id" element={<UsersEdit />} />
+                      <Route path="show/:id" element={<UsersShow />} />
+                    </Route>
+                    {/* <Route path="/reports">
                     <Route index element={< ReportList />} />
                     <Route path="create" element={< ReportCreate />} />
                     <Route path="edit/:id" element={< ReportEdit />} />
                     <Route path="show/:id" element={< ReportShow />} />
                   </Route> */}
-          
-                  <Route path="/comments">
-                    <Route index element={< CommentList />} />
-                    <Route path="create" element={< CommentCreate />} />
-                    <Route path="edit/:id" element={< CommentEdit />} />
-                    <Route path="post/:id" element={< CommentShow />} />
-                  </Route>
-                  <Route path="/forums">
-                    <Route index element={< ForumList />} />
-                    <Route path="create" element={< ForumCreate />} />
-                    <Route path="edit/:id" element={< ForumEdit />} />
-                    <Route path="show/:id" element={< ForumShow />} />
-                  </Route>
-                  <Route path="*" element={<ErrorComponent />} />
-                </Route>
-                <Route
-                  element={
-                    <Authenticated key="auth-pages" fallback={<Outlet />}>
-                      <NavigateToResource />
-                    </Authenticated>
-                  }
-                >
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/forgot-password" element={<ForgotPassword />} />
-                </Route>
-              </Routes>
 
-              <RefineKbar />
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
-            </Refine>
+                    <Route path="/comments">
+                      <Route index element={<CommentList />} />
+                      <Route path="create" element={<CommentCreate />} />
+                      <Route path="edit/:id" element={<CommentEdit />} />
+                      <Route path="post/:id" element={<CommentShow />} />
+                    </Route>
+                    <Route path="/forums">
+                      <Route index element={<ForumList />} />
+                      <Route path="create" element={<ForumCreate />} />
+                      <Route path="edit/:id" element={<ForumEdit />} />
+                      <Route path="show/:id" element={<ForumShow />} />
+                    </Route>
+                    <Route path="*" element={<ErrorComponent />} />
+                  </Route>
+                  <Route
+                    element={
+                      <Authenticated key="auth-pages" fallback={<Outlet />}>
+                        <NavigateToResource />
+                      </Authenticated>
+                    }
+                  >
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route
+                      path="/forgot-password"
+                      element={<ForgotPassword />}
+                    />
+                  </Route>
+                </Routes>
+
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
             </ThemeProvider>
           </RefineSnackbarProvider>
         </ColorModeContextProvider>
---
+++ Admin/src/pages/dashboard/index.tsx
@@ -0,0 +1,57 @@
+import { Typography } from "@mui/material";
+import Comments from "./comments";
+
+export default function DashboardPage() {
+  return (
+    <main>
+      <Typography sx={{ color: "#FFCA28" }}>
+        <h1>Our dashboard</h1>
+        <Comments />
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col md:flex-row  gap-4">
+            <div className="flex flex-col gap-4 h-full md:w-2/6">
+              <Comments />
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4  md:w-4/6">
+              <Comments />
+            </div>
+          </div>
+          <div className="flex flex-rown flex-wrap md:flex-nowrap gap-4">
+            <div className="w-full md:w-2/4">
+              <Comments />
+            </div>
+            <div className="w-full md:w-2/4">
+              <Comments />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
+            <div className="col-span-1 md:col-span-2">
+              <Comments />
+            </div>
+            <div className="col-span-1 md:col-span-1">
+              <Comments />
+            </div>
+            <div className="col-span-1 md:col-span-2">
+              <Comments />
+            </div>
+            <div className="col-span-1 md:col-span-2">
+              <Comments />
+            </div>
+            <div className="col-span-1 md:col-span-1">
+              <Comments />
+            </div>
+            <div className="col-span-1 md:col-span-2">
+              <Comments />
+            </div>
+            <div className="col-span-1 md:col-span-2">
+              <Comments />
+            </div>
+            <div className="col-span-1 md:col-span-3">
+              <Comments />
+            </div>
+          </div>
+        </div>
+      </Typography>
+    </main>
+  );
+}
