8f15dc46baf2063955f132d253ac5a65d803e832
STAGIAIRE2 <STAGIAIRE2@nutritechagro.com>
2024-04-02 16:59:21 +0000

    feat: reports chart

--- Admin/src/pages/dashboard/repports.tsx
+++ Admin/src/pages/dashboard/repports.tsx
@@ -4,7 +4,30 @@ import { PieChart } from "@mui/x-charts";
 
 const Repports = () => {
   return (
-   
+    <div>
+      <Typography sx={{ color: "#FFCA28" }}>
+        <h1>liked and repported posts</h1>
+      </Typography>
+      <Card variant="outlined">
+        <PieChart
+          colors={["red", "yellow", "green"]}
+          series={[
+            {
+              data: [
+                { id: 0, value: 10, label: "Reports" },
+                { id: 1, value: 15, label: "Likes" },
+              ],
+              innerRadius: 30,
+              outerRadius: 77,
+              paddingAngle: 4,
+              cornerRadius: -3,
+            },
+          ]}
+          width={500}
+          height={400}
+        />{" "}
+      </Card>
+    </div>
   );
 };
 export default Repports;
