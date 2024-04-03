6027b484f4094fdc6b5fc1efbd5d29c7a6f7fe5b
omazoz <oumaymamazoz12@gmail.com>
2024-04-03 02:42:24 +0000

    feat: number of tags chart

---
+++ Admin/src/pages/dashboard/tags.tsx
@@ -0,0 +1,25 @@
+import Card from "@mui/material/Card";
+import { LineChart } from '@mui/x-charts/LineChart';
+import { Typography } from '@mui/material';
+export default function Tags() {
+  return (
+    <main>
+        <Typography sx={{ color: "#FFCA28" }}>
+    <h1>Number of tags by month</h1>
+    </Typography>
+    <Card variant="outlined">
+    <LineChart
+      xAxis={[{ data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }]}
+      
+      series={[
+        {
+        color: "#FFCA28",
+          data: [2, 3, 5.5, 8.5, 1.5, 5, 1, 4, 3, 8],
+          showMark: ({ index }) => index % 2 === 0,
+        },
+      ]}
+      width={500}
+      height={300}
+    /></Card></main>
+  );
+}
