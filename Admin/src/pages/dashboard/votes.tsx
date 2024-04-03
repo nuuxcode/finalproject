b44d2a018ec257587533841666534e5296bec39b
omazoz <oumaymamazoz12@gmail.com>
2024-04-03 03:34:09 +0000

    feat: upvotes and downvotes  chart

--- Admin/src/pages/dashboard/votes.tsx
+++ Admin/src/pages/dashboard/votes.tsx
@@ -1,35 +1,34 @@
+import { Typography } from "@mui/material";
 import Card from "@mui/material/Card";
-import { Typography } from '@mui/material';
-import * as React from 'react';
-import { PieChart } from '@mui/x-charts/PieChart';
+import { PieChart } from "@mui/x-charts";
 
-const data = [
- 
-  { label: 'Downvotes', value: 200 },
-  { label: 'Upvotes', value: 278 },
-
-];
-
-export default function Votes() {
+const Votes = () => {
   return (
-    <main>
-    <Typography sx={{ color: "#FFCA28" }}>
+    <div>
+      <Typography sx={{ color: "#FFCA28" }}>
     <h1>Upvotes and Downvotes percentage</h1>
     </Typography>
     <Card variant="outlined">
     <PieChart
      colors={[ "yellow", "#FFCA28"]}
-      series={[
-      
-        {
-          startAngle: -90,
-          endAngle: 90,
-          data,
-        },
-      ]}
-      height={300}
-    />
-</Card>
-    </main>
+        
+          series={[
+            {
+              data: [
+                { id: 0, value: 74, label: "Upvotes" },
+                { id: 1, value: 15, label: "Downvotes" },
+              ],
+              innerRadius: 30,
+              outerRadius: 77,
+              paddingAngle: 4,
+              cornerRadius: -3,
+            },
+          ]}
+          width={500}
+          height={400}
+        />{" "}
+      </Card>
+    </div>
   );
-}
+};
+export default Votes;
