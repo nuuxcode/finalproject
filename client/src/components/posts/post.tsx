5c00f63da6777d06267c355ddf73aa5218d4dae6
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-04-03 22:11:02 +0000

    feat: add functionality and improve code readability in page, post, and fetcher components

--- client/src/components/posts/post.tsx
+++ client/src/components/posts/post.tsx
@@ -9,121 +9,134 @@ import { useUser, useClerk } from "@clerk/clerk-react";
 
 import Image from "next/image";
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-  } from "~/components/ui/popover";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
 import { ForumPost } from "~/types/Forum";
 import Link from "next/link";
 
 interface PostProps {
-    post: ForumPost
+  post: ForumPost
 }
 
-export default function Post({post}: PostProps) {
+export default function Post({ post }: PostProps) {
   const { isSignedIn } = useUser();
-  const {openSignIn} = useClerk()
+  const { openSignIn } = useClerk()
 
 
-    return (
-        <div className="dark:bg-secondary dark:text-[#d8dce0] bg-white w-full py-3 px-4 shadow border rounded-md">
-          <div className="flex justify-between items-center">
-            <div className="flex gap-2 items-center py-2">
-              <Link href={`/${post.user.username}`}>
-              <Image
-                className="rounded-full h-8 w-8 md:w-10 md:h-10"
-                src={post?.user.avatarUrl}
-                alt="logo"
-                width={40}
-                height={400}
-              />
-              </Link>
-              <div>
-              <Link href={`/forum/${post.forum.slug}`} className="dark:text-primary text-sm hover:underline cursor-pointer">{post.forum.name}</Link>
-              {/* <p className="text-primary text-sm hover:underline cursor-pointer">{post.user.username}</p> */}
-              </div>
-            </div>
-            <Popover>
-              <PopoverTrigger>
-                <FaEllipsisH className="text-" size={16} />
-              </PopoverTrigger>
-              <PopoverContent className="dark:bg-secondary border-0 max-w-[100px]">
-                <div className="flex flex-col gap-2">
-                  <button className="text-primary">Report</button>
-                </div>
-              </PopoverContent>
-            </Popover>
+  return (
+    <div className="dark:bg-secondary dark:text-[#d8dce0] bg-white w-full py-3 px-4 shadow border rounded-md">
+      <div className="flex justify-between items-center">
+        <div className="flex gap-2 items-center py-2">
+          <Link href={`/${post.user.username}`}>
+            <Image
+              className="rounded-full h-8 w-8 md:w-10 md:h-10"
+              src={post?.user?.avatarUrl || `https://eu.ui-avatars.com/api/?name=${encodeURIComponent(post?.user?.username)}&size=250`}
+              alt="logo"
+              width={40}
+              height={400}
+            />
+          </Link>
+          <div className="flex flex-col justify-center">
+            <Link href={`/forum/${post.forum.slug}`} className="dark:text-primary text-sm hover:underline cursor-pointer">
+              <span className="dark:text-yellow-500">Forum: </span>{post.forum.name}
+            </Link>
+            <Link
+              href={`/${post.user.username}`}
+              className="dark:text-primary hover:text-primary font-[500] text-xs hover:underline cursor-pointer"
+            >
+              <span className="dark:text-yellow-500">Author: </span>{post.user.username}
+            </Link>
           </div>
-          <p className=" py-2 leading-6 text-sm">
-            {post.title}
-          </p>
-          {post.attachments.map((attachment) => (
-            <div key={attachment.id} className="h-[300px] md:h-[500px] w-full">
-              <Image
-                src={attachment.url}
-                className="object-cover place-self-center w-full h-full rounded-[8px]"
-                alt="logo"
-                width={500}
-                height={500}
-              />
-
-            </div>))}
-            
-          {/* Not logged in btns */}
-          {!isSignedIn && (
-            <div className="flex justify-between items-center pt-4 gap-1 px-2">
-              <div className="flex gap-1 md:gap-4 items-center">
-                <div className="flex items-center gap-2">
-                  <FaRegHeart className="" />
-                  <p className="py- leading-6 text-sm">
-                    {post.votesCount}
-                  </p>
-                </div>
-                <div className="flex items-center gap-2">
-                  <FaCommentDots className="" />
-                  <p className="py- leading-6 text-sm">
-                    {post.commentsCount}
-                  </p>
-                </div>
-                <div className="flex items-center gap-1">
-                  <BiRepost size={24} className="" />
-                  <p className="py- leading-6 text-sm">
-                    {post.viewsCount}
-                  </p>
-                </div>
-              </div>
-                <div>
-                  <button onClick={() => openSignIn({
-                            afterSignInUrl: '/',
-                        })} className="underline text-sm">
-                    Log in to see more
-                  </button>
-                </div>
-            </div>
-          )}
-          {isSignedIn && <div className="flex justify-between items-center pt-4 px-2 dark:text-[#d8dce0]">
-            <div className="flex gap-4 items-center">
-              <div className="flex items-center gap-2 bg-[#64748B] bg-opacity-30 rounded-[40px] p-1">
-                <PiArrowFatLineUp className="text-secondary dark:text-[#d8dce0] cursor-pointer" />
-                <p className="text-secondary dark:text-[#d8dce0] leading-6 text-sm">
-                  {post.votesCount}
-                </p>
-                <PiArrowFatLineDown className="text-secondary dark:text-[#d8dce0] cursor-pointer" />
-              </div>
-              <div className="flex items-center gap-2 bg-[#64748B] bg-opacity-30 rounded-[40px] p-1 px-2">
-                <FaCommentDots className="text-secondary dark:text-[#d8dce0]" />
-                <p className="text-secondary leading-6 text-sm dark:text-[#d8dce0]">
-                  {post.commentsCount}
-                </p>
-              </div>
-              <div className="flex items-center gap-1 bg-[#64748B] bg-opacity-30 rounded-[40px] p-1">
-                <BiRepost size={24} className="text-secondary dark:text-[#d8dce0]" />
-                <p className="text-secondary leading-6 text-sm dark:text-[#d8dce0]">
-                  {post.viewsCount}
-                </p>
-              </div>
-            </div>
-          </div>}
         </div>
-    )
+        <Popover>
+          <PopoverTrigger>
+            <FaEllipsisH className="text-" size={16} />
+          </PopoverTrigger>
+          <PopoverContent className="dark:bg-secondary border-0 max-w-[100px]">
+            <div className="flex flex-col gap-2">
+              <button className="text-primary">Report</button>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+      <p className="py-2 leading-6 text-sm font-bold">
+        {post.title}
+      </p>
+      <p className="py-2 leading-6 text-sm line-clamp-2">
+        {post.content}
+      </p>
+      <Link href={`/post/${post.id}`} className="text-blue-500 hover:underline">
+        Read more
+      </Link>
+      {post.attachments.map((attachment) => (
+        <div key={attachment.id} className="h-[300px] md:h-[500px] w-full">
+          <Image
+            src={attachment.url}
+            className="object-cover place-self-center w-full h-full rounded-[8px]"
+            alt="logo"
+            width={500}
+            height={500}
+          />
+
+        </div>))}
+
+      {/* Not logged in btns */}
+      {!isSignedIn && (
+        <div className="flex justify-between items-center pt-4 gap-1 px-2">
+          <div className="flex gap-1 md:gap-4 items-center">
+            <div className="flex items-center gap-2">
+              <FaRegHeart className="" />
+              <p className="py- leading-6 text-sm">
+                {post.votesCount}
+              </p>
+            </div>
+            <div className="flex items-center gap-2">
+              <FaCommentDots className="" />
+              <p className="py- leading-6 text-sm">
+                {post.commentsCount}
+              </p>
+            </div>
+            <div className="flex items-center gap-1">
+              <BiRepost size={24} className="" />
+              <p className="py- leading-6 text-sm">
+                {post.viewsCount}
+              </p>
+            </div>
+          </div>
+          <div>
+            <button onClick={() => openSignIn({
+              afterSignInUrl: '/',
+            })} className="underline text-sm">
+              Log in to see more
+            </button>
+          </div>
+        </div>
+      )}
+      {isSignedIn && <div className="flex justify-between items-center pt-4 px-2 dark:text-[#d8dce0]">
+        <div className="flex gap-4 items-center">
+          <div className="flex items-center gap-2 bg-[#64748B] bg-opacity-30 rounded-[40px] p-1">
+            <PiArrowFatLineUp className="text-secondary dark:text-[#d8dce0] cursor-pointer" />
+            <p className="text-secondary dark:text-[#d8dce0] leading-6 text-sm">
+              {post.votesCount}
+            </p>
+            <PiArrowFatLineDown className="text-secondary dark:text-[#d8dce0] cursor-pointer" />
+          </div>
+          <div className="flex items-center gap-2 bg-[#64748B] bg-opacity-30 rounded-[40px] p-1 px-2">
+            <FaCommentDots className="text-secondary dark:text-[#d8dce0]" />
+            <p className="text-secondary leading-6 text-sm dark:text-[#d8dce0]">
+              {post.commentsCount}
+            </p>
+          </div>
+          <div className="flex items-center gap-1 bg-[#64748B] bg-opacity-30 rounded-[40px] p-1">
+            <BiRepost size={24} className="text-secondary dark:text-[#d8dce0]" />
+            <p className="text-secondary leading-6 text-sm dark:text-[#d8dce0]">
+              {post.viewsCount}
+            </p>
+          </div>
+        </div>
+      </div>}
+    </div>
+  )
 }
