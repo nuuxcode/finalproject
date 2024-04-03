fcd53082dd3eaa2fb9e834d3984e87417c5de2d4
omazoz <oumaymamazoz12@gmail.com>
2024-04-03 03:57:48 +0000

    fix error :forums chart

--- Admin/src/pages/dashboard/forums.tsx
+++ Admin/src/pages/dashboard/forums.tsx
@@ -6,7 +6,7 @@ import { Typography } from '@mui/material';
 const chartSetting = {
   xAxis: [
     {
-      label: 'rainfall (mm)',
+      label: 'months',
     },
   ],
   width: 500,
@@ -14,86 +14,60 @@ const chartSetting = {
 };
 const dataset = [
   {
-    london: 59,
-    paris: 57,
-    newYork: 86,
+    
     forums: 21,
     month: 'Jan',
   },
   {
-    london: 50,
-    paris: 52,
-    newYork: 78,
+   
     forums: 28,
     month: 'Fev',
   },
   {
-    london: 47,
-    paris: 53,
-    newYork: 106,
+    
     forums: 41,
     month: 'Mar',
   },
   {
-    london: 54,
-    paris: 56,
-    newYork: 92,
+    
     forums: 73,
     month: 'Apr',
   },
-  {
-    london: 57,
-    paris: 69,
-    newYork: 92,
+ {
     forums: 99,
     month: 'May',
   },
   {
-    london: 60,
-    paris: 63,
-    newYork: 103,
+   
     forums: 144,
     month: 'June',
   },
   {
-    london: 59,
-    paris: 60,
-    newYork: 105,
+ 
     forums: 319,
     month: 'July',
   },
   {
-    london: 65,
-    paris: 60,
-    newYork: 106,
     forums: 249,
     month: 'Aug',
   },
   {
-    london: 51,
-    paris: 51,
-    newYork: 95,
+    
     forums: 131,
     month: 'Sept',
   },
   {
-    london: 60,
-    paris: 65,
-    newYork: 97,
+  
     forums: 55,
     month: 'Oct',
   },
   {
-    london: 67,
-    paris: 64,
-    newYork: 76,
+  
     forums: 48,
     month: 'Nov',
   },
   {
-    london: 61,
-    paris: 70,
-    newYork: 103,
+   
     forums: 25,
     month: 'Dec',
   },
