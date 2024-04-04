bf66efb429fe5ee6aebe18c60917e142961eefa0
victor langat <provicml@gmail.com>
2024-04-05 02:45:40 +0300

    feat: Add ThemedTitleV2 component to Login page

--- Admin/src/pages/login/index.tsx
+++ Admin/src/pages/login/index.tsx
@@ -1,9 +1,14 @@
-import { AuthPage } from "@refinedev/mui";
+import { AuthPage, ThemedTitleV2 } from "@refinedev/mui";
 
 export const Login = () => {
   return (
     <AuthPage
       type="login"
+      title={
+        <ThemedTitleV2
+          text="Admin"
+          icon={<img src="./logo.png" />} collapsed={false}        />
+      }
       formProps={{
         defaultValues: {
           email: "admin@wgt.live",
